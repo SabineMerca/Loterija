2ed53d4fa38d21a627c5f026d6edbb93c0b2bb1e
SabineMerca <sabinemerca@inbox.lv>
2022-01-20 15:04:02 +0200

    otrais

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert(1);
+const vardi=['Jana Zilupe', 'Ilze Gulbe', 'Edmunds Mūks', 'Letīcija Zepa', 'Jānis Priede']
+const balvas=['Telefons', 'Auto', 'Māja', 'Putekļusūcējs', 'Dators']
+const naudaKopa=1000000; //kopeja summa
+let uzvaretajuSkaits=5;
+let rand= Math.random()*5;
+rand= Math.floor(rand); //noapaļo uz leju
+console.log(vardi[rand]); //izvada konsolē
