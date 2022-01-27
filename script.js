72c2b31dd13e7202f7d3d26dc7aed69abd240bee
SabineMerca <sabinemerca@inbox.lv>
2022-01-27 15:12:46 +0200

    kakis

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const vardi=['Jana Zilupe', 'Ilze Gulbe', 'Edmunds Mūks', 'Letīcija Zepa', 'Jānis Priede']
 const balvas=['Telefons', 'Auto', 'Māja', 'Putekļusūcējs', 'Dators']
 const naudaKopa=1000000; //kopeja summa
+
 let uzvaretajuSkaits=5;
+let rindas = document.querySelector('.rindas');
+rindas.innerHTML = ' ';
+for (let i = 0; i<uzvaretajuSkaits; i++){
 let rand= Math.random()*5;
+
 rand= Math.floor(rand); //noapaļo uz leju
-console.log(vardi[rand]); //izvada konsolē
+let uzvaretajs = vardi[rand]; //izvada konsole
+rindas.innerHTML += `
+<tr>
+<td>${i+1}</td>
+<td>${uzvaretajs}</td>
+</tr>`
+}
